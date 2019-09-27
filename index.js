89f2f6308058974e83d294d7534e5bffd1e46e53
pramodmhn <maharjanpramod@hotmail.com>
2019-09-27 12:41:31 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World from Pramod!')
+  response.send('Hello World from Pramod Test!')
 })
 
 app.listen(app.get('port'), function() {
